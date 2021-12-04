f3bb04f9d6fee24be8abc595337ee950f08e7c3b
Chimexx <mezie.cjo@gmail.com>
2021-12-04 09:23:29 +0100

    adjusted order model

--- models/Order.js
+++ models/Order.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema(
 	{
-		userId: { type: String, required: true },
-		products: [{ productId: { type: String }, quantity: { type: Number, default: 1 } }],
+		items: { type: Object },
 		amount: { type: Number, required: true },
 		shipping: { type: Object, required: true },
+		instructions: { type: String },
 		status: { type: String, default: "pending" },
 	},
 	{ timestamps: true }
